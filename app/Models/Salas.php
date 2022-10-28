0ead0c80a0bc34b88d08e529585740a076359cef
MAIKOL\HP <mgarrido2503@gmail.com>
2022-10-28 13:20:14 +0200

    m

--- app/Models/Salas.php
+++ app/Models/Salas.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Salas extends Model
-{
-    protected $table = "salas";
+{ 
+    use HasFactory;
+    // protected $table = "salas";
 }
