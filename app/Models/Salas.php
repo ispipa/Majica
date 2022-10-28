45dd25b9dc3347cdd40c921c44a73670e3d215d7
ispipa <ispipa123@gmail.com>
2022-10-28 14:42:51 +0200

    api

--- app/Models/Salas.php
+++ app/Models/Salas.php
@@ -6,13 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Salas extends Model
-<<<<<<< HEAD
-{ 
-    use HasFactory;
-    // protected $table = "salas";
-=======
 {
     use HasFactory;
     //protected $table = "salas";
->>>>>>> d8425b74924acd14d1bfc17bda6f23dffbcfcca8
+
 }
