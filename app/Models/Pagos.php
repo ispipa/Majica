6c46718d803e4450fed7353ff0e8fb2e8e6fe9fc
ispipa <ispipa123@gmail.com>
2022-10-28 11:00:47 +0200

    api

--- app/Models/Pagos.php
+++ app/Models/Pagos.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Salas extends Model
+class Pagos extends Model
 {
     use HasFactory;
-    //protected $table = "salas";
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuario', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nombre')->nullable();
+            $table->string('correo')->unique();
+            $table->string('contraseña')->nullable();
+            $table->string('apellidos')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('codigo_postal')->nullable();
+            $table->string('direccion')->nullable();
+
         });
     }
 
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuario');
     }
 };
