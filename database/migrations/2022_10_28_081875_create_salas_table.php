a8f4737ff7aa3c63f51f8b7cd14b54e36583b362
ispipa <ispipa123@gmail.com>
2022-11-07 11:07:02 +0100

    api

--- database/migrations/2022_10_28_081875_create_salas_table.php
+++ database/migrations/2022_10_28_081875_create_salas_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('nombre_sala',50);
             $table->string('descripcion_sala',50);
-            $table->enum('precio_sala',['0','1','2'])->index();
+            $table->set('precio_sala',['0','1','2'])->index();
             $table->enum('activo',['true','false']);
             $table->foreignId('piso')->constrained('pisos');
             $table->timestamps();
