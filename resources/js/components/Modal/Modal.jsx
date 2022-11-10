4f3df4750a5a7a464eef0db09bf2705a2d7a0e05
Raúl Torres <99418611+rauledutorres@users.noreply.github.com>
2022-11-10 09:20:03 +0100

    cambio use state contadorCompra

--- resources/js/components/Modal/Modal.jsx
+++ resources/js/components/Modal/Modal.jsx
@@ -24,7 +24,7 @@ const Modal = ({ id, disponibilidad, precio1, precio2, verModal, volver, setVerM
     const [errorr, setError] = useState(false);
     const [mostrarTabla, setMostratTabla] = useState(true);
     const [registros, setRegistros] = useState(obtenerRegistros());
-    const [contadorCompra, setContadorCompra] = useState(JSON.parse(localStorage.getItem("datos")).length);
+    const [contadorCompra, setContadorCompra] = useState(0);
 
     //ALMACENO LOS DATOS EN UNA VARIABLE (...REGISTROS)---
     const setDatos = () => {
